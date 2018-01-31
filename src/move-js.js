5afd8eb48e9c968eebf3a3ed3196d1641d62f83f
qiqiboy <imqiqiboy@gmail.com>
2018-01-31 19:51:33 +0800

    开发构建

--- src/move-js.js
+++ src/move-js.js
@@ -1,46 +1,29 @@
 /**
- * @overview
- *  用来方便的调用css3动画，支持transition和animation动画
+ * @overview 用来方便的调用css3动画，支持transition和animation动画
+ *
+ * @class Movejs
  */
 
-(function(root, factory) {
-    if (typeof define === 'function' && define.amd) {
-        define(factory);
-    } else if (typeof exports === 'object') {
-        module.exports = factory();
-    } else {
-        root.Movejs = factory();
-    }
-})(this, function() {
-    class Movejs {
-        constructor(node) {
-            if (!this instanceof Movejs) {
-                return new Movejs(node);
-            }
-
-            
-        }
-
-        querySelector(node) {
-            
-        }
-
-        start = () => {
-
-        }
-
-        stop = () => this.reset();
-
-        reset = () => {
-            this.setStyle({
-                transitionDuration: 0
-            })
-        }
-
-        then() {
-
+class Movejs {
+    constructor(node) {
+        if (!this instanceof Movejs) {
+            return new Movejs(node);
         }
     }
 
-    return Movejs;
-});
+    querySelector(node) {}
+
+    start = () => {};
+
+    stop = () => this.reset();
+
+    reset = () => {
+        this.setStyle({
+            transitionDuration: 0
+        });
+    };
+
+    then() {}
+}
+
+module.exports = Movejs;
